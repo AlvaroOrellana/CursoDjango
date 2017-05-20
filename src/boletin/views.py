ba41a8912f4d1b4b43e264412ed71a754ffa2920
alvaro <alvaroorellana9@hotmail.com>
2017-05-20 20:54:15 +0200

    Lectura 21

--- src/boletin/views.py
+++ src/boletin/views.py
@@ -49,7 +49,7 @@ def contact(request):
 		asunto = 'Form de Contacto'
 		email_from = settings.EMAIL_HOST_USER
 		email_to = [email_from, "otroemail@gmail.com"]
-		email_mensaje = "%s: %s enviado por %s" %(form_nombre, form_mensaje, form_email)
+		email_mensaje = "%s: %s enviado por %s" %(form_nombre,form_mensaje,form_email)
 		send_mail(asunto,
 			email_mensaje,
 			email_from,
